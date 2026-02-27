df61d92c06a34d43ac4ff6af8f092708b19baa8e
Arthur Augusto <voidhorn.rino@gmail.com>
2026-02-27 19:53:32 -0300

    removendo pastas desnecessárias

--- projeto_basico/cadastro_info.py
+++
@@ -1,3 +0,0 @@
-tempo = int(input('Quantos anos você tem? '))
-print('Você ainda é bem jovem' if tempo <= 45 else "Você já está ficando velho!")
-print('---Fim---')
--- testes_ADS/testes_ads.py
+++
@@ -1,3 +0,0 @@
-nome = 'guilherme'
-print(nome, nome, nome)
-print(f'Eu conheço um amigo chamado {nome}')
